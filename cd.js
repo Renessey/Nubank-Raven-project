9d31c8f58eaec96dbdd6b3876e95b9b02554d5b5
Renessey <144975138+Renessey@users.noreply.github.com>
2024-05-03 17:00:04 -0300

    nubank project

---
+++ cd.js
@@ -0,0 +1,10 @@
+var offset = $('#meuMenu').offset().top;
+var $meuMenu = $('#meuMenu');
+
+$(document).on('scroll', function () {
+    if (offset <= $(window).scrollTop()) {
+        header.addClass('fixar');
+    } else {
+        header.removeClass('fixar');
+    }
+});
